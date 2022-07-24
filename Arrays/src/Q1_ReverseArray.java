498cb80fb4e144fdab402d48c641dae815653826
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-24 16:48:12 +0530

    Add files via upload

---
+++ Arrays/src/Q1_ReverseArray.java
@@ -0,0 +1,15 @@
+public class Q1_ReverseArray {
+    public static void main(String[] args) {
+        // Reversing an Array
+        int arr[] = {1,2,3,4,5};
+        reverse(arr);
+    }
+
+    static void reverse(int arr[]){
+        // we are traversing an array in reverse order!!!
+        for (int i = arr.length-1; i >=0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
